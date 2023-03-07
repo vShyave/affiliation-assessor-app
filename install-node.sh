8118d9d4d7cfd0e49bd257ed66f7e818c14e5acd
ChakshuGautam <chaks.gautam@gmail.com>
2023-03-07 12:32:06 +0530

    Fix: issues with setup

--- install-node.sh
+++ install-node.sh
@@ -9,11 +9,3 @@ nvm install 16
 nvm use 14
 
 wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-
-# cd ./packages/enketo-core && npm i
-# cd ..
-# cd ./packages/enketo-express && docker run --name enketo-redis-main -p 6381:6379 -d redis && docker run --name enketo-redis-cache -p 6382:6379 -d redis && npm i && npm i -g grunt
-# cd ..
-# cd packages/form-manager && npm i
-# cd ..
-# cd ./apps/wrapper && npm i
