414f2615b1be50562ae79d2971931e949d3d2d64
Vinod Shyave <vinod.shyave@tarento.com>
2023-04-01 11:59:09 +0530

    today's assessment completed

--- apps/wrapper/src/pages/MedicalAssessments.jsx
+++ apps/wrapper/src/pages/MedicalAssessments.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ROUTE_MAP from "../routing/routeMap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faBuilding, faUser, faPhone, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+
 import Button from "../components/Button";
 import CommonLayout from "../components/CommonLayout";
+
 import { getMedicalAssessments } from "../api";
-import { faUser, faLock, faMobile } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StateContext } from "../App";
-import ROUTE_MAP from "../routing/routeMap";
 import { getCookie } from "../utils";
 
 const MedicalAssessments = () => {
@@ -39,17 +42,22 @@ const MedicalAssessments = () => {
         id: ass.institute.id,
         district: ass.institute.district,
         instituteName: ass.institute.name,
-        specialization:
-          ass.institute?.institute_specializations?.[0]?.specializations,
+        specialization: ass.institute?.institute_specializations?.[0]?.specializations,
         courses: ass.institute?.institute_types?.[0]?.types,
         type: ass.institute.sector,
         pocs: ass.institute.institute_pocs,
+        address: ass.institute.address,
         latitude: ass.institute.latitude,
         longitude: ass.institute.longitude,
       });
     } else setData(null);
     setLoading(false);
   };
+
+  const handleNavigate = () => {
+    alert(`lat - ${data.latitude} && long - ${data.longitude}`);
+  }
+
   useEffect(() => {
     getTodayAssessments();
     const {
@@ -60,119 +68,102 @@ const MedicalAssessments = () => {
   }, []);
 
   return (
-    <CommonLayout back={ROUTE_MAP.root}>
-      <div className="flex flex-col px-5 py-8 items-center">
-        <p className="text-secondary text-[28px] font-bold mt-4 lg:text-[45px] animate__animated animate__fadeInDown">
-          Today's Assessments
-        </p>
-        <p className="text-primary text-2xl font-bold animate__animated animate__fadeInDown">
-          {data?.district}
-        </p>
-        {!loading && data && (
-          <div className="h-full w-full bg-tertiary flex flex-col wrap items-center pt-4 pb-8 px-5 mt-4 animate__animated animate__fadeIn animate__slow overflow-scroll">
-            <div className="flex flex-col py-3 w-full ">
-              <span className="text-secondary pb-2 font-medium">
-                Institute Name
-              </span>
-              <input
-                type="text"
-                className="border-2 border-primary p-3.5"
-                disabled
-                value={data?.instituteName}
-              />
-            </div>
-            <div className="flex flex-col py-3 w-full">
-              <span className="text-secondary pb-2 font-medium">
-                Institute Type
-              </span>
-              <input
-                type="text"
-                disabled
-                className="border-2 border-primary p-3.5"
-                value={data?.type}
-              />
-            </div>
-            <div className="flex flex-col py-3 w-full">
-              <span className="text-secondary pb-2 font-medium">
-                POC Details
-              </span>
-              {data?.pocs?.map(el =>
-                <div className="mb-4 text-secondary"><FontAwesomeIcon icon={faUser} /> &nbsp; {el.name}, {el.number}</div>
-              )}
-            </div>
-            <div className="flex flex-col py-3 w-full">
-              <span className="text-secondary pb-2 font-medium">
-                Institute Specialization
-              </span>
-              <div className="flex flex-row gap-2 flex-wrap">
-                {data?.specialization?.map((el, idx) => (
-                  <span
-                    key={`${el}${idx}`}
-                    className="px-5 py-1 bg-primary rounded text-white"
-                  >
-                    {el}
-                  </span>
-                ))}
+    <CommonLayout back={ROUTE_MAP.root} logoutDisabled pageTitle="Today's Inspection" iconType="backArrow">
+      <div className={`flex flex-col px-6 h-full ${!data?.id ? 'justify-center' : '' }` }>
+        {
+          !loading && data && (
+            <div className="w-full bg-tertiary flex flex-col p-7 lg:w-[90%] rounded-[8px] animate__animated animate__fadeIn animate__slow overflow-scroll">
+              <div className="flex flex-col pb-4">
+                <div className="flex flex-row">
+                  <div>
+                    <FontAwesomeIcon icon={faBuilding} className="text-1xl lg:text-4xl text-gray-600" />
+                  </div>
+                  <div className="text-gray-500 ml-2">Applicant name</div>
+                </div>
+                <div className="mt-[4px] text-secondary text-[18px] font-medium">{ data?.instituteName }</div>
               </div>
-            </div>
-            {data?.courses?.length && (
-              <div className="flex flex-col py-3 w-full">
-                <span className="text-secondary pb-2 font-medium">
-                  Courses offered
-                </span>
-                <div className="flex flex-row gap-2 flex-wrap">
-                  {data?.courses?.map((el, idx) => (
-                    <span
-                      key={`${el}${idx}`}
-                      className="px-5 py-1 bg-primary rounded text-white"
-                    >
-                      {el}
-                    </span>
-                  ))}
+              <hr className="border-slate-400" />
+              <div className="flex flex-col py-4">
+                <div className="flex flex-row">
+                  <div>
+                    <FontAwesomeIcon icon={faLocationDot} className="text-1xl lg:text-4xl text-gray-600" />
+                  </div>
+                  <div className="text-gray-500 ml-2">District</div>
+                </div>
+                <div className="mt-[4px] text-secondary text-[18px] font-medium">{ data?.district }</div>
+              </div>
+              <hr className="border-slate-400" />
+              <div className="flex flex-col py-4">
+                <div className="flex flex-row">
+                  <div>
+                    <FontAwesomeIcon icon={faUser} className="text-1xl lg:text-4xl text-gray-600" />
+                  </div>
+                  <div className="text-gray-500 ml-2">POC Names</div>
+                </div>
+                { 
+                  data?.pocs?.map(el =>
+                    <div className="mt-[4px] text-secondary text-[18px] font-medium">{ el.name }</div>
+                  )
+                }
+              </div>
+              <hr className="border-slate-400" />
+              <div className="flex flex-col py-4">
+                <div className="flex flex-row">
+                  <div>
+                    <FontAwesomeIcon icon={faPhone} className="text-1xl lg:text-4xl text-gray-600" />
+                  </div>
+                  <div className="text-gray-500 ml-2">POC Numbers</div>
+                </div>
+                <div className="flex flex-row mt-[4px] wrap">
+                  { 
+                    data?.pocs?.map(el =>
+                      <div className="flex flex-col p-2 m-[4px] bg-[#DBDBDB;] grow items-center rounded-[8px]">
+                        <div className="rounded-[50%] w-[36px] h-[36px] bg-[#009A2B;] flex items-center justify-center">
+                          <FontAwesomeIcon icon={faPhone} className="text-1xl lg:text-4xl text-gray-600 text-white w-[16px]" />
+                        </div>
+                        <div className="mt-[4px] text-secondary text-[18px] font-medium">{ el.number }</div>
+                      </div>
+                    )
+                  }
                 </div>
               </div>
-            )}
-            <div className="flex flex-col py-3 w-full" >
-              <span className="text-secondary pb-2 font-medium">
-                Institute Location
-              </span>
-              {data.latitude && data.longitude && <iframe
-                src={`https://maps.google.com/maps?q=${data?.latitude},${data?.longitude}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
-                width={isMobile ? "100%" : "60%"}
-                height={200}
-                loading="lazy"
-                title="map"
-                className="mt-5 animate__animated animate__fadeIn"
-              />}
+              <hr className="border-slate-400" />
+              <div className="flex flex-col py-4">
+                <div className="flex flex-row">
+                  <div>
+                    <FontAwesomeIcon icon={faLocationDot} className="text-1xl lg:text-4xl text-gray-600" />
+                  </div>
+                  <div className="text-gray-500 ml-2">Address</div>
+                </div>
+                <div className="flex flex-row gap-7">
+                  <div className="flex grow mt-[4px]">
+                    <div className="text-secondary text-[18px] font-medium">
+                      { data.address }
+                    </div>
+                  </div>
+                  {
+                    data.latitude && data.longitude &&
+                    <div className="flex justify-center items-center" onClick={handleNavigate}>
+                      <div className="flex grow-0 w-[60px] h-[60px] bg-[#DBDBDB;] justify-center items-center">
+                        <FontAwesomeIcon icon={faLocationArrow} className="text-1xl lg:text-4xl text-gray-600 w-[24px] h-[24px]" />
+                      </div>
+                    </div>
+                  }
+                </div>
+              </div>
+              <Button text="Start Assessing" onClick={startAssess} />
             </div>
-            {/* <div className="flex flex-col py-3 w-full">
-              <span className="text-secondary pb-2 font-medium">
-                Is Nursing
-              </span>
-              <input
-                type="text"
-                disabled
-                className="border-2 border-primary p-3.5"
-                value={data?.nursing}
-              />
+          )
+        }
+        {
+          !loading && !data && (
+            <div className="flex flex-col">
+              <div className="w-full bg-tertiary flex flex-col p-7 lg:w-[90%] font-medium overflow-scroll rounded-[8px]">
+                <div className="text-secondary text-[24px] text-center">No Assessments Today</div>
+              </div>
             </div>
-            <div className="flex flex-col py-3 w-full">
-              <span className="text-secondary pb-2 font-medium">
-                Is Paramedical
-              </span>
-              <input
-                type="text"
-                disabled
-                className="border-2 border-primary p-3.5"
-                value={data?.paramedical}
-              />
-            </div> */}
-            <Button text="Start Assessing" onClick={startAssess} />
-          </div>
-        )}
-        {!loading && !data && (
-          <p className="text-3xl py-10">No Assessments Today</p>
-        )}
+          )
+        }
       </div>
     </CommonLayout>
   );
--- apps/wrapper/src/pages/UpcomingMedicalAssessments.jsx
+++ apps/wrapper/src/pages/UpcomingMedicalAssessments.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
+import ROUTE_MAP from "../routing/routeMap";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLocationDot, faCalendar, faCalendarDay, faCalendarAlt
-} from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 
 import CommonLayout from "../components/CommonLayout";
+
 import { getMedicalAssessmentsUpcoming } from "../api";
-import ROUTE_MAP from "../routing/routeMap";
 
 const UpcomingMedicalAssessments = () => {
   const [inspectionData, setInspectionData] = useState();
@@ -37,7 +37,7 @@ const UpcomingMedicalAssessments = () => {
                     </div>
                     <div className="text-gray-500 ml-2">District</div>
                   </div>
-                  <div className="mt-2 text-secondary text-[18px]">{ el.institute?.district || 'NA' }</div>
+                  <div className="mt-2 text-secondary text-[18px] font-medium">{ el.institute?.district || 'NA' }</div>
                 </div>
                 <hr className="border-slate-400" />
                 <div className="flex flex-col pt-4">
@@ -47,14 +47,14 @@ const UpcomingMedicalAssessments = () => {
                     </div>
                     <div className="text-gray-500 ml-2">Scheduled on</div>
                   </div>
-                  <div className="mt-2 text-secondary text-[18px]">{ el.date || 'NA' }</div>
+                  <div className="mt-2 text-secondary text-[18px] font-medium">{ el.date || 'NA' }</div>
                 </div>
               </div>
             })
           ) : (
             <div className="flex flex-col">
               <div className="w-full bg-tertiary flex flex-col p-7 lg:w-[90%] font-medium overflow-scroll rounded-[8px]">
-                <div className="text-secondary text-[24px] text-center">No data found!</div>
+                <div className="text-secondary text-[24px] text-center font-medium">No upcoming assessments found!</div>
               </div>
             </div>
           )
