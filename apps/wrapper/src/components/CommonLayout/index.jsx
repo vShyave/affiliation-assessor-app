a5989829fe77aa7d686c109d2b0ab4ed18a0b1e9
Vinod Shyave <vinod.shyave@tarento.com>
2023-04-02 13:33:04 +0530

    UI is ready for Welcome assessor

--- apps/wrapper/src/components/CommonLayout/index.jsx
+++ apps/wrapper/src/components/CommonLayout/index.jsx
@@ -28,8 +28,8 @@ const CommonLayout = (props) => {
   return (
     <>
       <div className="bg-tertiary h-screen w-screen flex flex-col lg:w-[52vw] md:w-[80vw] md:m-auto lg:m-auto">
-        <div className="w-full flex h-[18%] flex-row justify-between relative">
-          <div style={{ height: 20, width: 20, borderRadius: '50%', position: 'absolute', top: 0, right: 0, background: online ? '#229225' : 'red', marginTop: 10, marginRight: 20 }}></div>
+        <div className="w-full flex flex-row justify-between relative">
+          <div style={{ height: 20, width: 20, borderRadius: '50%', position: 'absolute', top: 0, right: 0, background: online ? '#229225' : 'red', marginTop: 20, marginRight: 20 }}></div>
           <img
             src="/assets/redGolLogo.png"
             className="p-5 h-[120px] w-[120px] lg:w-[170px] lg:h-[170px]"
@@ -41,7 +41,7 @@ const CommonLayout = (props) => {
             alt="illustration"
           />
         </div>
-        <div className="bg-white h-full w-full rounded-t-[60px] overflow-y-auto pb-5">
+        <div className="bg-white h-[calc(100%-120px)] w-full rounded-t-[60px] overflow-y-auto">
           <div className="flex flex-row w-full px-8 py-7 items-center cursor-pointer">
             {!props.backDisabled && (
               <FontAwesomeIcon
