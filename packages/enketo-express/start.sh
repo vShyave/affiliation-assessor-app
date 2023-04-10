2fb44953279a50a237185d925c3a500950b650c2
Tushar <codingid6@gmail.com>
2023-04-10 20:38:34 +0000

    Fix start.sh and Dockerfile

--- packages/enketo-express/start.sh
+++ packages/enketo-express/start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-cp config/default-config.json tmp.json
 
-jq -r --arg ENCRYPTION_KEY $ENCRYPTION_KEY '."encryption key" |= $ENCRYPTION_KEY' tmp.json > config/default-config.json
-jq -r --arg LESS_SECURE_ENCRYPTION_KEY $LESS_SECURE_ENCRYPTION_KEY '."less secure encryption key" |= $LESS_SECURE_ENCRYPTION_KEY' config/default-config.json > config/default-config.json
-
-jq -r --arg REDIS_MAIN_HOST $REDIS_MAIN_HOST '."redis.main.host" |= $REDIS_MAIN_HOST' tmp.json > config/default-config.json
-jq -r --arg REDIS_MAIN_CACHE $REDIS_MAIN_PORT '."redis.main.port" |= $REDIS_MAIN_PORT' tmp.json > config/default-config.json
-
-jq -r --arg REDIS_CACHE_HOST $REDIS_CACHE_HOST '."redis.cache.host" |= $REDIS_CACHE_HOST' tmp.json > config/default-config.json
-jq -r --arg REDIS_CACHE_CACHE $REDIS_CACHE_PORT '."redis.cache.port" |= $REDIS_CACHE_PORT' tmp.json > config/default-config.json
-
-rm tmp.json
+echo "$(jq -r --arg ENCRYPTION_KEY $ENCRYPTION_KEY '."encryption key" |= $ENCRYPTION_KEY' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg LESS_SECURE_ENCRYPTION_KEY $LESS_SECURE_ENCRYPTION_KEY '."less secure encryption key" |= $LESS_SECURE_ENCRYPTION_KEY' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg REDIS_MAIN_HOST $REDIS_MAIN_HOST '.redis.main.host |= $REDIS_MAIN_HOST' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg REDIS_MAIN_PORT $REDIS_MAIN_PORT '.redis.main.port |= $REDIS_MAIN_PORT' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg REDIS_CACHE_HOST $REDIS_CACHE_HOST '.redis.cache.host |= $REDIS_CACHE_HOST' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg REDIS_CACHE_PORT $REDIS_CACHE_PORT '.redis.cache.port |= $REDIS_CACHE_PORT' config/default-config.json)" > config/default-config.json
+echo "$(jq -r --arg FORM_MANAGER_BASE_URI $FORM_MANAGER_BASE_URI '.formManagerBaseURI |= $FORM_MANAGER_BASE_URI' config/default-config.json)" > config/default-config.json
 
 node app.js
