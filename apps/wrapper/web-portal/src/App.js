b5dfe4ac4551d5ba0ba3ac8057ee14571851738a
Preksha Vijay <preksha.vijay@tarento.com>
2023-05-05 16:49:15 +0530

    my changes

--- apps/wrapper/web-portal/src/App.js
+++ apps/wrapper/web-portal/src/App.js
@@ -2,24 +2,41 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { createContext, useEffect, useState } from "react";
 
 import ADMIN_ROUTE_MAP from "./routes/adminRouteMap";
-import './App.css';
+import "./App.css";
 
 import Authenticate from "./login/Authenticate";
 import AdminLogin from "./login/AdminLogin";
 import EnterOtp from "./login/EnterOtp";
 import AdminSingUp from "./login/AdminSignUp";
 import LoginEnterOtp from "./login/LoginEnterOtp";
+import AdminOnGroundInspectionAnalysis from "./pages/ground-analysis/AdminOnGroundInspectionAnalysis";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={ADMIN_ROUTE_MAP.auth} element={ <Authenticate /> }>
-            <Route path={ADMIN_ROUTE_MAP.loginModule.registerotp} element={ <EnterOtp /> }></Route>
-            <Route path={ADMIN_ROUTE_MAP.loginModule.login} element={ <AdminLogin /> }></Route>
-            <Route path={ADMIN_ROUTE_MAP.loginModule.signup} element={ <AdminSingUp /> }></Route>
-            <Route path={ADMIN_ROUTE_MAP.loginModule.loginotp} element={ <LoginEnterOtp /> }></Route>
+          <Route path={ADMIN_ROUTE_MAP.auth} element={<Authenticate />}>
+            <Route
+              path={ADMIN_ROUTE_MAP.loginModule.registerotp}
+              element={<EnterOtp />}
+            ></Route>
+            <Route
+              path={ADMIN_ROUTE_MAP.loginModule.login}
+              element={<AdminLogin />}
+            ></Route>
+            <Route
+              path={ADMIN_ROUTE_MAP.loginModule.signup}
+              element={<AdminSingUp />}
+            ></Route>
+            <Route
+              path={ADMIN_ROUTE_MAP.loginModule.loginotp}
+              element={<LoginEnterOtp />}
+            ></Route>
+            <Route
+              path={ADMIN_ROUTE_MAP.adminModule.onGroundInspectionAnalysis.list}
+              element={<AdminOnGroundInspectionAnalysis />}
+            ></Route>
           </Route>
         </Routes>
       </BrowserRouter>
--- apps/wrapper/web-portal/src/routes/adminRouteMap.js
+++ apps/wrapper/web-portal/src/routes/adminRouteMap.js
@@ -1,13 +1,18 @@
-
 let ADMIN_ROUTE_MAP = {
-    auth: '/auth'
+  auth: "/auth",
 };
 
 ADMIN_ROUTE_MAP.loginModule = {
-    login: `${ADMIN_ROUTE_MAP.auth}/login`,
-    registerotp: `${ADMIN_ROUTE_MAP.auth}/registerotp`,
-    signup: `${ADMIN_ROUTE_MAP.auth}/register`,
-    loginotp: `${ADMIN_ROUTE_MAP.auth}/loginotp`,
-}
+  login: `${ADMIN_ROUTE_MAP.auth}/login`,
+  registerotp: `${ADMIN_ROUTE_MAP.auth}/registerotp`,
+  signup: `${ADMIN_ROUTE_MAP.auth}/register`,
+  loginotp: `${ADMIN_ROUTE_MAP.auth}/loginotp`,
+};
+
+ADMIN_ROUTE_MAP.adminModule = {
+  onGroundInspectionAnalysis: {
+    list: "list",
+  },
+};
 
 export default ADMIN_ROUTE_MAP;
