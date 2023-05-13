d63c533745ec1baeb0c25da5532a607f3c641719
Vinod Shyave <vinod.shyave@tarento.com>
2023-05-13 13:10:47 +0530

    env variables changed

---
+++ apps/wrapper/utils/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className=''></button>
+  )
+}
+
+export default Button
---
+++ apps/wrapper/utils/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='flex flex-col rounded-[8px] bg-white w-[100] h-[100]'></div>
+  )
+}
+
+export default Card
