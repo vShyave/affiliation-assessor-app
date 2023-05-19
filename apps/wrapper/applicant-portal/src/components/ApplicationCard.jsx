e571cd310607ede2834af2f30d0db517b92035c8
devendra <devendra@devendras-MacBook-Pro.local>
2023-05-19 12:03:29 +0530

    Feat: application and form list api integration

--- apps/wrapper/applicant-portal/src/components/ApplicationCard.jsx
+++ apps/wrapper/applicant-portal/src/components/ApplicationCard.jsx
@@ -3,13 +3,13 @@ import { Card, Button } from './index';
 
 const ApplicationCard = (props) => {
     return  (
-        <Card moreClass="flex flex-col border-gray-100 m-3 gap-3 w-[300px] border-[1px] drop-shadow" key={props.application.form_id}>
+        <Card moreClass="flex flex-col border-gray-100 m-3 gap-3 w-[300px] border-[1px] drop-shadow">
             <div className='text-xl font-medium'>{props.application.form_name}</div>
             <div className='text-sm'>Submitted on: {props.application.submitted_on}</div>
             <div className='text-sm'> <span className='text-xs text-green-500 p-1 rounded-md' style={{backgroundColor: '#eee' }}>Status: {props.application.review_status}</span></div>
             
             <div className='flex'>
-                <Button moreClass="text-primary-500 font-bold uppercase border-gray-500 text-primary-400" style={{backgroundColor: '#fff' }} text="View Application "></Button>
+                <Button moreClass="text-primary-500 font-bold uppercase border-gray-500 text-primary-400" style={{backgroundColor: '#fff' }} text="View Application " onClick={props.onView ? () => props.onView(props.application.form_id) : null}></Button>
             </div>
         </Card>
     )
