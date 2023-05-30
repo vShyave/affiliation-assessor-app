19e9c6f5993bf7d7ff35954a7bac65644b4ac0ac
Preksha Vijay <preksha.vijay@tarento.com>
2023-05-30 11:32:53 +0530

    merge changes

--- apps/wrapper/web-portal/src/api/apiUrl.js
+++ apps/wrapper/web-portal/src/api/apiUrl.js
@@ -11,42 +11,22 @@ const API_URL = {
     acceptApplicant: `${rest}acceptForm`,
     rejectApplicant: `${rest}rejectForm`,
     getGroundInspectionAnalysis: `${rest}getGroundInspectionAnalysis`,
-    ViewStatus:{
-      getViewStatus: `${rest}getStatusLog`
-    }
-  
+    ViewStatus: {
+      getViewStatus: `${rest}getStatusLog`,
+    },
   },
   manageForms: {
-    convertODKtoXML: `user/convert`
+    convertODKtoXML: `user/convert`,
   },
   manageUsers: {
     userList: `${rest}getAllUsers`,
     specificUser: `${rest}getSpecificUser`,
   },
-<<<<<<< HEAD
-  acceptApplication: {
-    acceptApplicant: `${rest}acceptForm`,
-  },
-  rejectApplication: {
-    rejectApplicant: `${rest}rejectForm`,
-  },
-  groundInspectionAnalysis: {
-    getGroundInspectionAnalysis: `${rest}getGroundInspectionAnalysis`,
-  },
-  UsersForSchedulingAssessment: {
+
+  desktopAnalysis: {
     getUsersForSchedulingAssessment: "rest/getUsersForSchedulingAssessment",
-    ViewStatus: {
-      getViewStatus: `${rest}getStatusLog`,
-    },
+    scheduleAssessment: `rest/scheduleAssessment`,
   },
 };
-=======
-
-  desktopAnalysis:{
-    getUsersForSchedulingAssessment:"rest/getUsersForSchedulingAssessment",
-    scheduleAssessment: `rest/scheduleAssessment`
-  }
-}
->>>>>>> 82891716aae1cc80acf73eb91fb4fe14b17cafc6
 
 export default API_URL;
