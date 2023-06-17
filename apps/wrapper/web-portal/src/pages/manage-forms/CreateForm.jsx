43e5fa3891e7d6a2d7f7fc9d9c9a2e88aec98d21
Sheela Kumari <sheela.kumari@tarento.com>
2023-06-17 13:23:40 +0530

    manage forms include file path

--- apps/wrapper/web-portal/src/pages/manage-forms/CreateForm.jsx
+++ apps/wrapper/web-portal/src/pages/manage-forms/CreateForm.jsx
@@ -92,7 +92,7 @@ const CreateForm = () => {
     try {
       const res = await convertODKtoXML(postData);
       setXmlData(res.data);
-      setFormData((prevState) => ({ ...prevState, path: "www.google.com" }));
+      setFormData((prevState) => ({ ...prevState, path: res.data.fileUrl }));
       //TODO: function call to invoke API for uploading xml file and get the remote path
       //TODO: add remote path to formData (state).
       setToast((prevState) => ({
@@ -143,6 +143,7 @@ const CreateForm = () => {
         labels: formDetail?.labels,
         round_no: formDetail?.round,
         title: formDetail?.title,
+        path: formDetail?.path
       });
     } catch (error) {
       console.log("error - ", error);
@@ -375,14 +376,14 @@ const CreateForm = () => {
                 <div className="flex justify-end">
                   <button
                     className={`${
-                      Object.values(formData).length !== 6
+                      Object.values(formData).length < 6
                         ? "bg-gray-400 text-gray-700 cursor-not-allowed"
                         : "px-6 text-white bg-primary-500 border border-primary-500"
                     } border w-[140px] h-[40px] font-medium rounded-[4px] `}
                     style={{ backgroundColor: "" }}
                     type="submit"
                     disabled={
-                      Object.values(formData).length !== 6 ? true : false
+                      Object.values(formData).length < 6 ? true : false
                     }
                   >
                     Next
