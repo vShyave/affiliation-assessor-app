f3b691c4a1c6f45e94d140f270743ff637a4fdce
Sheela Kumari <sheela.kumari@tarento.com>
2023-08-01 14:41:53 +0530

    attribute name change

--- apps/wrapper/web-portal/src/components/Header.jsx
+++ apps/wrapper/web-portal/src/components/Header.jsx
@@ -62,7 +62,7 @@ export default function Header() {
                       aria-labelledby="menu-button"
                       tabIndex="-1"
                     >
-                      <div class="py-1" role="none">
+                      <div className="py-1" role="none">
                         <button
                           type="button"
                           className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
