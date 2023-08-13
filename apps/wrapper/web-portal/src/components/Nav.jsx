0ae21bb97af63c0b873a075224481acd33f43cdc
Sheela Kumari <sheela.kumari@tarento.com>
2023-08-13 13:34:12 +0530

    roles based login and access

--- apps/wrapper/web-portal/src/components/Nav.jsx
+++ apps/wrapper/web-portal/src/components/Nav.jsx
@@ -5,67 +5,67 @@ import "./Header.css";
 import { AiFillHome } from "react-icons/ai";
 
 import ADMIN_ROUTE_MAP from "../routes/adminRouteMap";
+import { getCookie } from "../utils";
 
 export default function Nav() {
-
   return (
     <>
-        <div className="h-[48px] bg-white drop-shadow-sm">
-          <div className="container px-3 mx-auto">
-            <div className="flex items-center h-[48px] py-2">
-              {/* <div className="flex bg-white hidden md:contents"> */}
-              <ul className="flex md nav-items text-gray-500 text-[14px] font-bold uppercase gap-4 justify-center align-center menus">
-                <li className="flex bg-primary-800 rounded-md items-center justify-center h-8 w-8 hover:cursor-pointer">
-                  <NavLink to={ADMIN_ROUTE_MAP.adminModule.manageUsers.home}>
-                    <AiFillHome className="text-white text-xl" />
-                  </NavLink>
-                </li>
-                {/* <li className="flex hover:text-primary-600 hover:cursor-pointer">
+      <div className="h-[48px] bg-white drop-shadow-sm">
+        <div className="container px-3 mx-auto">
+          <div className="flex items-center h-[48px] py-2">
+            {/* <div className="flex bg-white hidden md:contents"> */}
+            <ul className="flex md nav-items text-gray-500 text-[14px] font-bold uppercase gap-4 justify-center align-center menus">
+              <li className="flex bg-primary-800 rounded-md items-center justify-center h-8 w-8 hover:cursor-pointer">
+                <NavLink to={ADMIN_ROUTE_MAP.adminModule.manageUsers.home}>
+                  <AiFillHome className="text-white text-xl" />
+                </NavLink>
+              </li>
+              {/* <li className="flex hover:text-primary-600 hover:cursor-pointer">
                   <NavLink to={ADMIN_ROUTE_MAP.adminModule.dashboard}>Dashboard</NavLink>
                 </li> */}
+              {getCookie("regulator")[0]["role"] === "Super-Admin" && (
                 <li className="flex hover:text-primary-600 hover:cursor-pointer">
                   <NavLink to={ADMIN_ROUTE_MAP.adminModule.manageUsers.home}>
                     Manage Users
                   </NavLink>
                 </li>
-                <li className="flex hover:text-primary-600 hover:cursor-pointer">
-                  <NavLink to={ADMIN_ROUTE_MAP.adminModule.manageForms.home}>
-                    Manage Forms
-                  </NavLink>
-                </li>
-                <li className="flex hover:text-primary-600 hover:cursor-pointer">
-                  <NavLink
-                    to={ADMIN_ROUTE_MAP.adminModule.desktopAnalysis.home}
-                  >
-                    Desktop Analysis
-                  </NavLink>
-                </li>
-                <li className="flex hover:text-primary-600 hover:cursor-pointer">
-                  <NavLink
-                    to={ADMIN_ROUTE_MAP.adminModule.onGroundInspection.home}
-                  >
-                    On Ground Inspection Analysis
-                  </NavLink>
-                </li>
-                {/* <li className="flex hover:text-primary-600 hover:cursor-pointer">
+              )}
+              <li className="flex hover:text-primary-600 hover:cursor-pointer">
+                <NavLink to={ADMIN_ROUTE_MAP.adminModule.manageForms.home}>
+                  Manage Forms
+                </NavLink>
+              </li>
+              <li className="flex hover:text-primary-600 hover:cursor-pointer">
+                <NavLink to={ADMIN_ROUTE_MAP.adminModule.desktopAnalysis.home}>
+                  Desktop Analysis
+                </NavLink>
+              </li>
+              <li className="flex hover:text-primary-600 hover:cursor-pointer">
+                <NavLink
+                  to={ADMIN_ROUTE_MAP.adminModule.onGroundInspection.home}
+                >
+                  On Ground Inspection Analysis
+                </NavLink>
+              </li>
+              {/* <li className="flex hover:text-primary-600 hover:cursor-pointer">
                   <NavLink
                     to={ADMIN_ROUTE_MAP.adminModule.certificateManagement.home }
                   >
                     Certificate Management
                   </NavLink>
                 </li> */}
-                <li className="flex hover:text-primary-600 hover:cursor-pointer">
-                  <NavLink
-                    to={ADMIN_ROUTE_MAP.adminModule.scheduleManagement.home}
-                  >
-                    Schedule Management
-                  </NavLink>
-                </li>
-              </ul>
-              
-              {/* </div> */}
+              <li className="flex hover:text-primary-600 hover:cursor-pointer">
+                <NavLink
+                  to={ADMIN_ROUTE_MAP.adminModule.scheduleManagement.home}
+                >
+                  Schedule Management
+                </NavLink>
+              </li>
+            </ul>
 
-              {/* <ul className={`nav-items duration-500 font-bold md:hidden flex flex-col text-gray-600 p-4 bg-gray-200 fixed w-4/5 h-screen left-0 top-[100px] gap-10 ${toggle ? "left-[0%]" : "left-[-100%]"} `}>
+            {/* </div> */}
+
+            {/* <ul className={`nav-items duration-500 font-bold md:hidden flex flex-col text-gray-600 p-4 bg-gray-200 fixed w-4/5 h-screen left-0 top-[100px] gap-10 ${toggle ? "left-[0%]" : "left-[-100%]"} `}>
                                 <Link to="/">
                                     <li className="bg-primary-800 rounded-md flex items-center justify-center h-8 w-8 hover:cursor-pointer"><AiFillHome className="text-white text-xl"/></li>
                                 </Link>
@@ -96,7 +96,7 @@ export default function Nav() {
                                 )
                             } */}
 
-              {/* <div className="container mx-auto h-[64px] flex justify-between items-center">
+            {/* <div className="container mx-auto h-[64px] flex justify-between items-center">
                 <div className="flex bg-white hidden md:contents">
                     <ul className="flex md nav-items text-gray-500 gap-4">
                         <Link to="/">
@@ -154,11 +154,9 @@ export default function Nav() {
                     )
                 }
             </div> */}
-            </div>
           </div>
         </div>
-      
-      
+      </div>
     </>
   );
 }
