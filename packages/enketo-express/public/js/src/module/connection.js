886f543d118be7a8fcf622e496fecd3a6f403f60
devendra <devendra@devendras-MacBook-Pro.local>
2023-08-30 14:05:59 +0530

    Feat: Enketo Offline- Changes around formSpec

--- packages/enketo-express/public/js/src/module/connection.js
+++ packages/enketo-express/public/js/src/module/connection.js
@@ -152,13 +152,13 @@ async function _uploadBatch(recordBatch, record) {
         controller.abort();
     }, settings.timeout);
 
-    function getMeta(metaName) {
-        return document.querySelector(`meta[name=${metaName}]`).content;
-    }
+    // function getMeta(metaName) {
+    //     return document.querySelector(`meta[name=${metaName}]`).content;
+    // }
 
-    // const formSpec = getMeta('formSpec');
+    // const formSpec = getMeta('formSpec') ;
     const formController = new FormController();
-    const response = await formController.processForm(record.xml, record.files);
+    const response = await formController.processFormNew(record.xml, record.files);
     // TODO: transforrm the form response
     // TODO: submit the form
     formController.broadcastFormData();
