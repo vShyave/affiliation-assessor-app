5ffefcac5d8e95b0c99bfd90df0696f97eb94e62
Sheela Kumari <sheela.kumari@tarento.com>
2023-09-13 16:43:33 +0530

    store device ID at login

--- apps/wrapper/web-portal/src/login/AdminLogin.jsx
+++ apps/wrapper/web-portal/src/login/AdminLogin.jsx
@@ -127,21 +127,10 @@ const AdminLogin = () => {
 
       //setting device ID
       if (getCookie("firebase_client_token") !== undefined) {
-        let deviceIds =
-          JSON.parse(adminDetailsRes.data.regulator[0].device_id) || [];
-        if (!deviceIds.includes(getCookie("firebase_client_token"))) {
-          console.log(deviceIds);
-          console.log(deviceIds.includes(getCookie("firebase_client_token")));
-          deviceIds.push(getCookie("firebase_client_token"));
-          console.log({
-            user_id: loginRes.data.userRepresentation.id,
-            device_id: JSON.stringify(deviceIds),
-          });
-          await updateRegulatorDeviceId({
-            user_id: loginRes.data.userRepresentation.id,
-            device_id: JSON.stringify(deviceIds),
-          });
-        }
+        await updateRegulatorDeviceId({
+          user_id: getCookie("userData")?.userRepresentation?.id,
+          device_id: JSON.stringify([getCookie("firebase_client_token")]),
+        });
       }
     } catch (error) {
       setToast((prevState) => ({
